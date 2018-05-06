41c5c8a08f442a194a9f5d21250afd1a03d5054a
hly920928 <hly920928@gmail.com>
2018-05-07 01:53:02 +0800

    WPF_CG_2D_TestBed

---
+++ MyTest_WPF/MyTest_WPF/Testbed2D/GraphPaper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicsBook
+{
+    public class GraphPaper : Canvas
+    {
+
+    }
+}
